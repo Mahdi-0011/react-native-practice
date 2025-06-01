d021e4c96f62ff36712c9ac0ed16645aa6bb6f1c
Mahdi <mahdi.mousavi0011@gmail.com>
2025-06-01 14:07:32 +0200

    add input and btn with styles.

--- App.tsx
+++ App.tsx
@@ -1,79 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TextInput, Button } from "react-native";
+import React from "react";
 
-export default function App() {
+export default function App () {
   return <>
-    <SafeAreaView>
-    <SafeAreaView style={styles.container} edges={['top']}>
-      <ScrollView style={styles.scrollView}>
     <View style={styles.container}>
-
-      <View style={styles.view1}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Hello this is me</Text>
-      <StatusBar style="auto" />
-      </View>
-      <View style={styles.view2}></View>
-      <View style={styles.view3}></View>
-      <View style={styles.view4}></View>
-            <View style={styles.view1}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Hello this is me</Text>
-      <StatusBar style="auto" />
-      </View>
-      <View style={styles.view2}></View>
-      <View style={styles.view3}></View>
-      <View style={styles.view4}></View>
-            <View style={styles.view1}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Hello this is me</Text>
-      <StatusBar style="auto" />
-      </View>
-      <View style={styles.view2}></View>
-      <View style={styles.view3}></View>
-      <View style={styles.view4}></View>
-            <View style={styles.view1}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Hello this is me</Text>
-      <StatusBar style="auto" />
-      </View>
-      <View style={styles.view2}></View>
-      <View style={styles.view3}></View>
-      <View style={styles.view4}></View>
+      <ScrollView>
+        <View style={styles.form}>
+          <TextInput 
+          style={styles.input}
+          placeholder="your input is ready...">
+          </TextInput>
+          <View style={styles.btn}>
+          <Button title={"Add"}
+          />
+          </View>
+        </View>
+      </ScrollView>
     </View>
-          </ScrollView>
-    </SafeAreaView>
-  </SafeAreaView>
- </>;
-}
+  </>
+};
 
 const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    height: 1000, 
-    backgroundColor: 'blue',
-    padding: 2,
-    
-
-    flexDirection: 'column',
-  },
-  view1: {
-    flex: 2,
-    backgroundColor: '#eee',
-        alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view2: {
-    flex: 2,
-    backgroundColor: '#fcc',
-  },
-  view3: {
-    flex: 2,
-    backgroundColor: 'orange',
-  },
-  view4: {
-    flex: 3,
+  container:{
+    flex: 1,
+    paddingTop: 70,
+    margin: 1,
     backgroundColor: 'skyblue',
   },
+  form: {
+    flexDirection: 'row',
+    height: 300,
+    backgroundColor: 'darkseagreen',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  input: {
+    width: 250,
+    height: 50,
+    backgroundColor: 'orange',
+    marginLeft: 20,
+    padding: 15,
+    borderRadius:15,
+  },
+  btn: {
+    width: 90,
+    height: 50,
+    backgroundColor: 'yellow',
+    padding: 2,
+    marginLeft: 10,
+    borderRadius: 20,
+    justifyContent: 'center',
+  }
 });
