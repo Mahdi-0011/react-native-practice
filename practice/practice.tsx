8cd49513638428b9863e7b479e1c883df844a728
Mahdi <mahdi.mousavi0011@gmail.com>
2025-06-01 14:07:12 +0200

    add practice file.

---
+++ practice/practice.tsx
@@ -0,0 +1,77 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return <>
+    <SafeAreaView>
+      <ScrollView>
+    <View style={styles.container}>
+
+      <View style={styles.view1}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello this is me</Text>
+      <StatusBar style="auto" />
+      </View>
+      <View style={styles.view2}></View>
+      <View style={styles.view3}></View>
+      <View style={styles.view4}></View>
+            <View style={styles.view1}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello this is me</Text>
+      <StatusBar style="auto" />
+      </View>
+      <View style={styles.view2}></View>
+      <View style={styles.view3}></View>
+      <View style={styles.view4}></View>
+            <View style={styles.view1}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello this is me</Text>
+      <StatusBar style="auto" />
+      </View>
+      <View style={styles.view2}></View>
+      <View style={styles.view3}></View>
+      <View style={styles.view4}></View>
+            <View style={styles.view1}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello this is me</Text>
+      <StatusBar style="auto" />
+      </View>
+      <View style={styles.view2}></View>
+      <View style={styles.view3}></View>
+      <View style={styles.view4}></View>
+    </View>
+          </ScrollView>
+    </SafeAreaView>
+ </>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    height: 1000, 
+    backgroundColor: 'blue',
+    padding: 2,
+    
+
+    flexDirection: 'column',
+  },
+  view1: {
+    flex: 2,
+    backgroundColor: '#eee',
+        alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view2: {
+    flex: 2,
+    backgroundColor: '#fcc',
+  },
+  view3: {
+    flex: 2,
+    backgroundColor: 'orange',
+  },
+  view4: {
+    flex: 3,
+    backgroundColor: 'skyblue',
+  },
+});
